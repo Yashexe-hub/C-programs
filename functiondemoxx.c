5254a8b7c30d38f810001bea07f24b6e68d96a3b
Yash Thakur <yashthakur200529@gmail.com>
2026-04-25 12:16:13 +0530

    Add files via upload

---
+++ functiondemoxx.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void display()
+{
+    printf("Jay ganesh...\n");
+}
+int main()
+{
+    display();
+    display();
+    display();
+    
+    return 0;
+}
---
+++ return.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int display()
+{
+    return 11;
+}
+int main()
+{
+    int ret = 0;
+
+    ret = display();
+    
+    printf("%d\n", ret);
+    
+    return 0;
+}
