3dd7e02490febc3082330c33872f99f1350fb958
Yash Thakur <yashthakur200529@gmail.com>
2026-05-05 23:31:23 +0530

    Add files via upload

---
+++ Close_file.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+int main()
+{
+    int fd = 0;
+
+    fd = open("Marvellous.txt",O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file");
+    }
+    else
+    {
+        printf("File gets closed with fd : %d\n",fd);
+    }
+
+    close(fd);
+    return 0;
+}
---
+++ Unlink_file.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+int main()
+{
+    unlink("Marvellous.txt");
+
+    return 0;
+}
